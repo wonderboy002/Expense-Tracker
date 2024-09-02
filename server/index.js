5aef6c962c51614798ed7d05fb472ba6ffb83cab
wonderboy002 <nipunkup@gmail.com>
2024-09-02 21:31:20 +0530

    added env files

--- server/index.js
+++ server/index.js
@@ -2,10 +2,11 @@ import express from "express";
 import mongoose from "mongoose";
 import ExpenseRouter from "./Routes/Expense.routes.js";
 import cors from "cors";
+import dotenv from "dotenv";
 
 const app = express();
 
-
+dotenv.config();
 const port = 3001;
 app.use(express.json());
 app.use(cors());
@@ -18,7 +19,9 @@ app.get("/",(req,res)=>{
   res.send("Welcome Home!!!");
 })
 
-const mongoURI="mongodb+srv://nipunkup:njzCslzwJ7vgrAwB@cluster0.531tq.mongodb.net/";
+const mongoURI=process.env.MONGO_URI;
+
+
 
 mongoose.connect(mongoURI)
 .then(()=>console.log("Successfully Connected to mongodb"))
